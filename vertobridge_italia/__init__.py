d212be24af6c2363eff2c6b9ca78535f879899d8
Sarino Alfonso Grande <129679366+sag1687@users.noreply.github.com>
2026-06-08 23:35:24 +0200

    Delete vertobridge_italia directory

--- vertobridge_italia/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""QGIS plugin entry point for VertoBridge Italia."""
-
-
-def classFactory(iface):
-    from .plugin import VertoBridgeItaliaPlugin
-
-    return VertoBridgeItaliaPlugin(iface)
